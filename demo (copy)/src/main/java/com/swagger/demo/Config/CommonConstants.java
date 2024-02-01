6aafd87527fb42f0ef04d72dfd597f9f64c45d99
jayparikh1912 <158047848+jayparikh1912@users.noreply.github.com>
2024-02-01 18:01:40 +0530

    Add files via upload
    scheduler executes at 5 pm and get admin details like email for sending mail of total latest userDetails ,first it create pdf and then send... to the admin

---
+++ demo (copy)/src/main/java/com/swagger/demo/Config/CommonConstants.java
@@ -0,0 +1,13 @@
+package com.swagger.demo.Config;
+
+public class CommonConstants {
+    public static final String SERVER_EMAIL_ADDRESS = "server_email_address";
+    public static final String SERVER_EMAIL_PASSWORD = "server_email_password";
+    public static final String SERVER_EMAIL_PORT = "server_email_port";
+
+
+
+//    public static final String SERVER_EMAIL_ADDRESS = "server_email_address";
+//    public static final String SERVER_EMAIL_ADDRESS = "server_email_address";
+//    public static final String SERVER_EMAIL_ADDRESS = "server_email_address";
+}
