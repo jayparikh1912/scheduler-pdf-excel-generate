bdcd47c8967dff8d6e3da67984f8018686ba3bcb
jayparikh1912 <158047848+jayparikh1912@users.noreply.github.com>
2024-02-01 18:09:47 +0530

    Delete demo (copy) directory

--- demo (copy)/src/main/java/com/swagger/demo/User/SystemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.swagger.demo.User;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SystemRepository extends JpaRepository<SystemSetting,Long> {
-    SystemSetting findByKeyAndIsDeletedIsFalse(String key);
-}
